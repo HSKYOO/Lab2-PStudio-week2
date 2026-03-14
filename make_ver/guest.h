f88c3838a69f5da77e10b45e78182507cf4cc6a0
HSKYOO <hyunseok888@gmail.com>
2026-03-14 17:19:13 +0900

    d

--- make_ver/guest.h
+++
@@ -1,4 +0,0 @@
-#include <stdio.h>
-
-int addGuest();
-void displayGuest(int menu);
--- make_ver/menu.c
+++
@@ -1,8 +0,0 @@
-#include "menu.h"
-
-void displayMenu(){
-    printf("**********************\n");
-    printf("1. Pizza : 20000 \n");
-    printf("2. Pasta : 12000 \n");
-    printf("**********************\n");
-}
